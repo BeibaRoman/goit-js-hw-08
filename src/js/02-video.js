b184ae367e04081862e125ef8aa9b42c04020f1b
BeibaRoman <106346781+BeibaRoman@users.noreply.github.com>
2025-02-06 19:44:56 +0200

    done second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+// Імпортуємо плеєр з пакету @vimeo/player
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+// Функція для обробки оновлення часу
+const onPlay = function (data) {
+  save(STORAGE_KEY, data.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// Додати перевірку перед завантаженням часу
+const savedTime = load(STORAGE_KEY);
+
+if (savedTime !== undefined) {
+  player.setCurrentTime(savedTime).catch(error => {
+    console.error('Error setting current time:', error);
+  });
+}
